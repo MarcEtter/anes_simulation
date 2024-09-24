438c6a1dfcabdc073969d37e4ce8d49706888413
Morcupine <mje9832@nyu.edu>
2024-09-24 01:53:24 -0400

    fixed bug where election df vectors iteratively divided by sd

--- simulation.py
+++ simulation.py
@@ -3,16 +3,16 @@ import pandas as pd
 
 election = default.copy()
 if RUN_REGRESSIONS:#if we retabulate the data, save it to file
-    election['df'] = default['df'].copy()
+    election['df_init'] = default['df_init'].copy()
     for party in default['parties']:
-        for key in default['df'].keys():
+        for key in default['df_init'].keys():
             if party in key:
-                election['df'] = election['df'].drop(key, axis ='columns') 
-    election['df'].to_csv('simulation_data.csv')
+                election['df_init'] = election['df_init'].drop(key, axis ='columns') 
+    election['df_init'].to_csv('simulation_data.csv')
 else:   
     #Below df is trimmed to only necessary variables, but could leave extra variables in the simulation data csv
     #This would permit the simulation of historical elections where specifying fundamentals above would be redundant
-    election['df'] = pd.read_csv('simulation_data.csv')
+    election['df_init'] = pd.read_csv('simulation_data.csv')
 
 election['year'] = 2020
 election['inc_party_cand_approval'] = 0.44*100 #0.44 #Harris approval rating, according to 538
@@ -27,33 +27,28 @@ election['candidates'] = {
     'party': 'dem',
     'code': 0,
     'ideology': 2.1,
-    'poll': 0.1},
+    'poll': 0.49},
 
 'gop':
     {
     'party': 'gop',
     'code': 1,
     'ideology': 5.25,
-    'poll': 0.1},
+    'poll': 0.46},
 
 'indep':
     {'party': 'indep',
      'code': 2,
     'ideology': 4.25,
-    'poll': 0.1}
+    'poll': 0.05}
 }
 
 election['parties'] = [x for x in election['candidates'].keys()]
 election['party_codes'] = dict(zip(election['parties'], [x['code'] for x in election['candidates'].values()]))
 
-sim_df = election['df']
-sim_df = sim_df[sim_df['year'] == election['year']]
-sim_df['inc_party_cand_approval'] = election['inc_party_cand_approval']
-sim_df['inflation_yoy'] = election['inflation_yoy']
-sim_df['inc_party'] = election['inc_party']
-sim_df['inc_party_tenure'] = election['inc_party_tenure']
-sim_df['rdi_yr_to_election'] = election['rdi_yr_to_election']
-election['df'] = sim_df
+#sim_df = election['df']
+#dataframe vectors set to election variables above here previously
+#election['df'] = sim_df
 election['df'] = initialize(election)
 PARTIES = election['parties']
 model_fundamentals_dict = load_model_fundamentals_multi(REGRESSION_OBJ_PATH, election['parties'])
@@ -64,7 +59,7 @@ test = {}
 i = 1
 while i <= 7:
     election['candidates']['indep']['ideology'] = i#3.5 * (3.5**i) / (3.5**i + 1)
-    election['candidates']['gop']['ideology'] = 8-i if i <= 4 else 4#3.5 * (3.5**i + 1) / (3.5**i)
+    #election['candidates']['gop']['ideology'] = 8-i if i <= 4 else 4#3.5 * (3.5**i + 1) / (3.5**i)
     election['df'] = initialize(election)
     PARTIES = election['parties']
     election['df'] = simulate_election(election, model_fundamentals_dict)
@@ -83,3 +78,4 @@ for fips_code in sim_df['fips'].value_counts().keys():
 state_shares = pd.DataFrame(state_shares).transpose()
 state_shares['fips'] = state_shares.index
 state_shares['state'] = state_shares['fips'].apply(lambda x: state_name[x])
+state_shares.to_csv('state_shares.csv')
